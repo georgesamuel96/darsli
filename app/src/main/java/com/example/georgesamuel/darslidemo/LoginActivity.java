3ea1c27581913488aec4e841549845c37494cbde
android <mina.shaker>
2018-11-22 12:40:48 +0200

    add login logic

--- app/src/main/java/com/example/georgesamuel/darslidemo/LoginActivity.java
+++ app/src/main/java/com/example/georgesamuel/darslidemo/LoginActivity.java
@@ -1,13 +1,58 @@
 package com.example.georgesamuel.darslidemo;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
 
 public class LoginActivity extends AppCompatActivity {
+    EditText edtUserName;
+    EditText edtPassword;
+    Button btnLogin;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        initUI();
+    }
+
+    private void initUI() {
+        edtUserName = findViewById(R.id.edtUserName);
+        edtPassword = findViewById(R.id.edtPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                login();
+            }
+        });
+
+    }
+
+    void login() {
+        String userName = edtUserName.getText().toString().trim();
+        String password = edtPassword.getText().toString().trim();
+
+        if (userName.isEmpty()) {
+            edtUserName.setError("من فضلك قم باخال اسم المستخدم");
+        } else if (password.isEmpty()) {
+            edtPassword.setError("من فضلك قم باخال الرقم السري");
+        } else if (password.length() < 8) {
+            edtPassword.setError("من فضلك قم رقم سري صحيح");
+
+        } else {
+            navigateToHome();
+        }
+    }
+
+    private void navigateToHome() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+
     }
 }
