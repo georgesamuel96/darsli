14dfd9d7754f02a46bcec23bc3216b014d9cd220
android <mina.shaker>
2018-11-22 12:51:29 +0200

    navigate to teachers

--- app/src/main/java/com/example/georgesamuel/darslidemo/MaterialActivity.java
+++ app/src/main/java/com/example/georgesamuel/darslidemo/MaterialActivity.java
@@ -1,13 +1,51 @@
 package com.example.georgesamuel.darslidemo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.view.View;
 
 public class MaterialActivity extends AppCompatActivity {
+    CardView card1, card2, card3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_material);
+        initUI();
+
     }
+
+    private void initUI() {
+        card1 = findViewById(R.id.card1);
+        card1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                navigateToTeachers();
+            }
+        });
+        card2 = findViewById(R.id.card2);
+        card2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                navigateToTeachers();
+
+            }
+        });
+        card3 = findViewById(R.id.card3);
+        card3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                navigateToTeachers();
+
+            }
+        });
+    }
+
+    void navigateToTeachers() {
+        Intent intent = new Intent(this, TeacherActivity.class);
+        startActivity(intent);
+    }
+
 }
