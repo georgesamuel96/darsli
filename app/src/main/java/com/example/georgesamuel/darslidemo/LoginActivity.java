69a93ce05e0b208a7b12145a356fdc71cc1850dd
georgesamuel96 <georges012441@gmail.com>
2018-11-22 13:21:52 +0200

    sharedpref

--- app/src/main/java/com/example/georgesamuel/darslidemo/LoginActivity.java
+++ app/src/main/java/com/example/georgesamuel/darslidemo/LoginActivity.java
@@ -1,13 +1,29 @@
 package com.example.georgesamuel.darslidemo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class LoginActivity extends AppCompatActivity {
+    private SharedPrefConfig sharedPreferenceConfig;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        SharedPref();
+}
+
+    public void SharedPref()
+    {
+        sharedPreferenceConfig = new SharedPrefConfig(getApplicationContext());
+
+        if(sharedPreferenceConfig.getLoginStatus())
+        {
+            /*Intent i = new Intent(LoginActivity.this, homepage.class);
+            startActivity(i);*/
+            finish();
+        }
     }
 }
